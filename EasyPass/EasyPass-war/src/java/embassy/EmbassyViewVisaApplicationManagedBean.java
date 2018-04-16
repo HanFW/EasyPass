ee0c070f5bd0d384142c5e425426cb9570c2e561
Yongxue <yongxue0701@gmail.com>
2018-04-17 01:17:07 +0800

    integration - update transaction update visa status

--- EasyPass/EasyPass-war/src/java/embassy/EmbassyViewVisaApplicationManagedBean.java
+++ EasyPass/EasyPass-war/src/java/embassy/EmbassyViewVisaApplicationManagedBean.java
@@ -25,6 +25,7 @@ import objects.Citizen;
 import objects.CriminalRecord;
 import objects.Insurance;
 import objects.LocalContact;
+import objects.Passport;
 import objects.TransportationReference;
 import objects.Visa;
 import objects.VisaApplication;
@@ -49,6 +50,7 @@ public class EmbassyViewVisaApplicationManagedBean {
     private Insurance insurance;
     private LocalContact localContact;
     private CriminalRecord criminalRecord;
+    private Passport passport;
     private boolean hasVisa;
     private Visa visa;
 
@@ -76,58 +78,83 @@ public class EmbassyViewVisaApplicationManagedBean {
                     insurance = mapper.readValue(new File("/Users/hanfengwei/Desktop/IS4302/project/data/Asset/Insurance/get_response_byId.json"), Insurance.class);
                     localContact = mapper.readValue(new File("/Users/hanfengwei/Desktop/IS4302/project/data/Asset/LocalContact/get_response_byId.json"), LocalContact.class);
                     criminalRecord = mapper.readValue(new File("/Users/hanfengwei/Desktop/IS4302/project/data/Asset/CriminalRecord/get_response_byId.json"), CriminalRecord.class);
+
                     if (visaStatus.getStatusState().equals(Constants.APPLICATION_STATUS_APPROVED)) {
                         hasVisa = true;
                         visa = mapper.readValue(new File("/Users/hanfengwei/Desktop/IS4302/project/data/Asset/Visa/get_response_byId.json"), Visa.class);
                     } else {
                         hasVisa = false;
                     }
+
                 } else {
                     try {
-                        HttpResponse<JsonNode> citizenResponse = Unirest.get("http://localhost:3000/api/org.acme.easypass.Citizen/" + visaApplication.getPassport())
+                        String[] citizenIdArray = visaApplication.getPassport().split("#");
+                        HttpResponse<JsonNode> citizenResponse = Unirest.get("http://localhost:3000/api/org.acme.easypass.Citizen/" + citizenIdArray[1])
                                 .header("accept", "application/json")
                                 .asJson();
                         citizen = mapper.readValue(citizenResponse.getBody().getObject().toString(), Citizen.class);
 
-                        HttpResponse<JsonNode> visaStatusResponse = Unirest.get("http://localhost:3000/api/org.acme.easypass.VisaStatus/" + visaApplication.getVisaStatus())
+                        String[] visaStatusIdArray = visaApplication.getVisaStatus().split("#");
+                        HttpResponse<JsonNode> visaStatusResponse = Unirest.get("http://localhost:3000/api/org.acme.easypass.VisaStatus/" + visaStatusIdArray[1])
                                 .header("accept", "application/json")
                                 .asJson();
                         visaStatus = mapper.readValue(visaStatusResponse.getBody().getObject().toString(), VisaStatus.class);
 
-                        HttpResponse<JsonNode> basicInfoResponse = Unirest.get("http://localhost:3000/api/org.acme.easypass.Citizen/" + visaApplication.getBasicInfo())
+                        String[] basicInfoIdArray = visaApplication.getBasicInfo().split("#");
+                        HttpResponse<JsonNode> basicInfoResponse = Unirest.get("http://localhost:3000/api/org.acme.easypass.BasicInfo/" + basicInfoIdArray[1])
                                 .header("accept", "application/json")
                                 .asJson();
                         basicInfo = mapper.readValue(basicInfoResponse.getBody().getObject().toString(), BasicInfo.class);
 
-                        HttpResponse<JsonNode> bankStatementResponse = Unirest.get("http://localhost:3000/api/org.acme.easypass.BankStatement/" + visaApplication.getBankStatement())
+                        String[] bankStatementIdArray = visaApplication.getBankStatement().split("#");
+                        HttpResponse<JsonNode> bankStatementResponse = Unirest.get("http://localhost:3000/api/org.acme.easypass.BankStatement/" + bankStatementIdArray[1])
                                 .header("accept", "application/json")
                                 .asJson();
                         bankStatement = mapper.readValue(bankStatementResponse.getBody().getObject().toString(), BankStatement.class);
 
-                        HttpResponse<JsonNode> transportationResponse = Unirest.get("http://localhost:3000/api/org.acme.easypass.TransportationReference/" + visaApplication.getTransportationReference())
+                        String[] transportationIdArray = visaApplication.getTransportationReference().split("#");
+                        HttpResponse<JsonNode> transportationResponse = Unirest.get("http://localhost:3000/api/org.acme.easypass.TransportationReference/" + transportationIdArray[1])
                                 .header("accept", "application/json")
                                 .asJson();
                         transportation = mapper.readValue(transportationResponse.getBody().getObject().toString(), TransportationReference.class);
 
-                        HttpResponse<JsonNode> accommodationResponse = Unirest.get("http://localhost:3000/api/org.acme.easypass.Accommodation/" + visaApplication.getAccommodation())
+                        String[] accommodationIdArray = visaApplication.getAccommodation().split("#");
+                        HttpResponse<JsonNode> accommodationResponse = Unirest.get("http://localhost:3000/api/org.acme.easypass.Accommodation/" + accommodationIdArray[1])
                                 .header("accept", "application/json")
                                 .asJson();
                         accommodation = mapper.readValue(accommodationResponse.getBody().getObject().toString(), Accommodation.class);
 
-                        HttpResponse<JsonNode> insuranceResponse = Unirest.get("http://localhost:3000/api/org.acme.easypass.Insurance/" + visaApplication.getInsurance())
+                        String[] insuranceIdArrayId = visaApplication.getInsurance().split("#");
+                        HttpResponse<JsonNode> insuranceResponse = Unirest.get("http://localhost:3000/api/org.acme.easypass.Insurance/" + insuranceIdArrayId[1])
                                 .header("accept", "application/json")
                                 .asJson();
                         insurance = mapper.readValue(insuranceResponse.getBody().getObject().toString(), Insurance.class);
 
-                        HttpResponse<JsonNode> localContactResponse = Unirest.get("http://localhost:3000/api/org.acme.easypass.LocalContact/" + visaApplication.getLocalContact())
+                        String[] localContactIdArray = visaApplication.getLocalContact().split("#");
+                        HttpResponse<JsonNode> localContactResponse = Unirest.get("http://localhost:3000/api/org.acme.easypass.LocalContact/" + localContactIdArray[1])
                                 .header("accept", "application/json")
                                 .asJson();
                         localContact = mapper.readValue(localContactResponse.getBody().getObject().toString(), LocalContact.class);
 
-                        HttpResponse<JsonNode> criminalRecordResponse = Unirest.get("http://localhost:3000/api/org.acme.easypass.CriminalRecord/" + visaApplication.getCriminalRecord())
+                        String[] criminalRecordIdArray = visaApplication.getCriminalRecord().split("#");
+                        HttpResponse<JsonNode> criminalRecordResponse = Unirest.get("http://localhost:3000/api/org.acme.easypass.CriminalRecord/" + criminalRecordIdArray[1])
                                 .header("accept", "application/json")
                                 .asJson();
                         criminalRecord = mapper.readValue(criminalRecordResponse.getBody().getObject().toString(), CriminalRecord.class);
+
+                        String[] passportIdArray = visaApplication.getPassport().split("#");
+                        HttpResponse<JsonNode> passportResponse = Unirest.get("http://localhost:3000/api/org.acme.easypass.Passport/" + passportIdArray[1])
+                                .header("accept", "application/json")
+                                .asJson();
+                        passport = mapper.readValue(passportResponse.getBody().getObject().toString(), Passport.class);
+
+                        if (visaStatus.getStatusState().equals(Constants.APPLICATION_STATUS_APPROVED)) {
+                            hasVisa = true;
+                            visa = passport.getVisas().get(0);
+                        } else {
+                            hasVisa = false;
+                        }
+
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
